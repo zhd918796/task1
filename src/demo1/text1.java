06fc718e6cafeffdc9f21b1707710967d50018b1
dudu <dudu@163.com>
2021-12-05 19:57:32 +0800

    third commit

--- src/demo1/text1.java
+++ src/demo1/text1.java
@@ -4,5 +4,6 @@ public class text1 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println(11);
+        System.out.println(22);
     }
 }
